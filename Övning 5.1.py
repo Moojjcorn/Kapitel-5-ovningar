89d73cb313c641cf8af5dfef966f40bf045f5f19
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2021-01-25 13:27:16 +0100

    Update Övning 5.1.py

--- Övning 5.1.py
+++ Övning 5.1.py
@@ -1,9 +1,10 @@
 import datetime
-dt = datetime.datetime.now()
-
-mån = dt.month
-dag = dt.day
 while True:
-    personnummer = input("Ange ditt personnummer ÅÅÅÅMMDD: ")
-    pn_mån = personnummer[4:6]
-    pn_dag = personnummer[6:8]
+    dt = datetime.datetime.now()
+    pernum = input("Skriv ditt personnummer ÅÅÅÅMMDD: ")
+    dtpers = dt.strptime(pernum, '%Y%m%d')
+
+    if (dt.month == dtpers.month and dt.day == dtpers.day):
+        print("Grattis på födelsedagen")
+    else: 
+        print("Du fyller inte år idag")
