16590697ccc428bae17b7008feeb7ca96f2e1448
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2021-01-25 14:34:09 +0100

    Update Övning 5.8.py

--- Övning 5.8.py
+++ Övning 5.8.py
@@ -1,2 +1,12 @@
-Time1 = input("Skriv in en tid på dygnet i formatet 'TT:MM': ")
-Time2 = input("Skriv in en til tid på dygnet som är efter den första, i formatet 'TT:MM': ")
+while True:
+    Time1 = input("\nSkriv in en tid på dygnet i formatet 'TT:MM': ")
+    Time2 = input("\nSkriv in en till tid på dygnet som är efter den första, i formatet 'TT:MM': ")
+
+    Time1Split = Time1.split(':')
+    Time2Split = Time2.split(':')
+    #Omvandla till int för att kunna räckna
+    #Veta hur man räcknar med specifikt en del av splitten
+    #Jag behöver omvandla allt till minuter
+    #Sedan subtrahere dem, den större med den mindre. dvs: Time2 - Time1
+    print(Time1Split)
+    print(Time2Split)
