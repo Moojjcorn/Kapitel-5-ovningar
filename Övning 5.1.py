0ae5cf7f41fd3fbba14bee508c9f50aca3bc4e06
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2021-01-25 13:41:46 +0100

    Update Övning 5.1.py

--- Övning 5.1.py
+++ Övning 5.1.py
@@ -1,10 +1,22 @@
+#Importerar modulen datetime
 import datetime
+
+#Skapar en loop som är True
 while True:
+    #Skapar en variabel "dt" som blir som en shortcut för datetime.datetime.now()
     dt = datetime.datetime.now()
+
+    #Tar en input från användaren och ber om personnummer
     pernum = input("Skriv ditt personnummer ÅÅÅÅMMDD: ")
+
+    #Skapar en ny variabel "dtpers" som har datetime.datetime.now().striptime som splittar ditt personnummer till formatet %Y%m%d
     dtpers = dt.strptime(pernum, '%Y%m%d')
 
+    #En If sats som kollar om dagens månad och dag stämmer med ditt personnumers angivna månad och dag 
     if (dt.month == dtpers.month and dt.day == dtpers.day):
+
+        #Printar vv om if satsen är sann
         print("Grattis på födelsedagen")
     else: 
+        #En else sats som printar vv om if satsen är falsk
         print("Du fyller inte år idag")
